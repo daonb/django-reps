caf3601c12ca287d69623591844f3588e796e501
Ory Band <oryband@gmail.com>
2011-01-31 21:06:26 +0200

    first schemae

--- reps/django_reps/models.py
+++ reps/django_reps/models.py
@@ -1,5 +1,22 @@
-from django.db.models import *
+from django.db import models 
+from django.contrib.auth.models import User
 
-class User(Model):
-    pass
+class UserProfile(models.Model):
+    since = models.DateField()
+    rep = models.ForeignKey(User, null=True, related_name='supporters')
 
+class Ballot(models.Model):
+    start_date = models.DateField()
+    d_day = models.DateField()
+    subject = models.TextField()
+    description = models.TextField()
+
+class Vote(models.Model):
+    ''' a specific vote by a specific user '''
+    voter = models.ForeignKey(User, related_name='votes')
+    vote = models.DecimalField()
+    why = models.TextField()
+    when = models.DateField()
+    what = models.ForeignKey(Ballot, related_names='votes')
+    # TODO: decide on a privacy policy
+    # is_public = models.BooleanField(default=False)
